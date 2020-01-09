adc1640cd264cc2af8354808e6d02b134c3b8495
doxelaustin <austin@doxel.ai>
2020-01-09 11:29:47 -0800

    master push

--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -9,6 +9,31 @@ class UsernameForm extends Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
     }
+    onSubmit(e) {
+        e.preventDefault()
+        this.props.onSubmit(this.state.username)
+    }
+    onChange(e){
+        this.setState({ username: e.target.value})
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <h2>What is your username</h2>
+                    <form onSubmit = {this.onSubmit}>
+                        <input
+                            type = "text"
+                            placeholder = "Your full name"
+                            onChange = {this.onChange}
+                            />
+                        <input type = "submit"/>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 
 }
 
